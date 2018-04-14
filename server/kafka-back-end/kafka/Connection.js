29683461a20f6bd074a26c699e8e5928b7a94793
Sasank <sasank196@gmail.com>
2018-04-13 21:12:09 -0700

    Added kafka Consumer and created topics for all the actions and used mongodb as the database.
    Signed-off-by: Sasank <sasank196@gmail.com>

--- server/kafka-back-end/kafka/Connection.js
+++ server/kafka-back-end/kafka/Connection.js
@@ -5,9 +5,16 @@ function ConnectionProvider() {
         if (!this.kafkaConsumerConnection) {
 
             this.client = new kafka.Client("localhost:2181");
-            this.kafkaConsumerConnection = new kafka.Consumer(this.client,[ { topic: topic_name, partition: 0 }]);
-            this.client.on('ready', function () { console.log('client ready!') })
+            //console.log(this.client);
+            //this.kafkaConsumerConnection = new kafka.Consumer(this.client,[ { topic: topic_name, partition: 0 }]);
+            //console.log(this.kafkaConsumerConnection);
+            this.kafkaConsumerConnection = new kafka.Consumer(this.client, topic_name);
+            this.client.on('ready', function () { 
+                console.log('client ready!');
+            });
+
         }
+
         return this.kafkaConsumerConnection;
     };
 
@@ -18,7 +25,7 @@ function ConnectionProvider() {
             this.client = new kafka.Client("localhost:2181");
             var HighLevelProducer = kafka.HighLevelProducer;
             this.kafkaProducerConnection = new HighLevelProducer(this.client);
-            this.kafkaConnection = new kafka.Producer(this.client);
+            //this.kafkaConnection = new kafka.Producer(this.client);
             console.log('producer ready');
         }
         return this.kafkaProducerConnection;
