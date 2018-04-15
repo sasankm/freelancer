a77207dfcf6a9a82892aed2badd153d32c701f52
Sasank <sasank196@gmail.com>
2018-04-14 22:05:51 -0700

    Done some modifications in server.
    Signed-off-by: Sasank <sasank196@gmail.com>

--- server/database/mysql.js
+++ server/database/mysql.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var pool  = mysql.createPool({
-    host     : 'mydbinstance.ce4iu9boor4m.us-west-1.rds.amazonaws.com',
+    host     : 'mydbinstance.ce4iu9boor4m.us-west-1.rds.amazonaws.com:3306',
     user     : 'sasank196',
     password : 'sasank196',
     database : 'freelancer1'
